a7686a8369678aa6d4b18f51be6dc3dbadb99dd5
Kevin Gisi <kevin@kevingisi.com>
2021-04-18 09:03:22 -0500

    Extracted drawing config logic to module

---
+++ modules/DrawingConfigManager.js
@@ -0,0 +1,35 @@
+class DrawingConfigManager {
+  constructor() {
+    Hooks.on("renderDrawingConfig", this.handleRenderDrawingConfig.bind(this));
+  }
+
+  async handleRenderDrawingConfig(_, html, data) {
+    // TODO: Permissions check
+    await this.injectTab(html, data);
+    this.createDropZones(html); 
+  }
+
+  async injectTab(html, data) {
+    renderTemplate('modules/hotspots/templates/drawing-config-hotspots-nav.hbs').then(markup => {
+      html.find('.tabs .item').last().after(navMarkup);
+    });
+    const tabMarkup = await renderTemplate('modules/hotspots/templates/drawing-config-hotspots-tab.hbs', data.object.flags.hotspots || {});
+    await html.find('.tab').last().after(tabMarkup);
+  }
+
+  createDropZones(html) {
+    (new DragDrop({
+      dropSelector: '.dropspot',
+      callbacks: { drop: this.handleDrop }
+    })).bind(html[0]);
+  }
+
+  handleDrop(event) {
+    const data = JSON.parse(event.dataTransfer.getData('text/plain'))
+    if (data.type === "Macro") {
+      console.log("Received macro", data);
+    }
+  }
+}
+
+export default DrawingConfigManager;
--- modules/index.js
+++ modules/index.js
@@ -1,45 +1,5 @@
-console.log("HOTSPOTS WAS LOADED");
+import DrawingConfigManager from './DrawingConfigManager.js';
 
-let lastDragged;
-
-async function _injectDrawingConfigTab(app, html, data) {
-  renderTemplate('modules/hotspots/templates/drawing-config-hotspots-nav.hbs').then(markup => {
-    html.find('.tabs .item').last().after(markup);
-  });
-  renderTemplate('modules/hotspots/templates/drawing-config-hotspots-tab.hbs', data.object.flags.hotspots || {}).then(markup => {
-    html.find('.tab').last().after(markup);
-  });
-}
-
-function _supportDragDrop(html) {
-  const dragDrop = new DragDrop({
-    dropSelector: ".dropspot",
-    callbacks: { 
-      drop(event) { 
-        const data = JSON.parse(event.dataTransfer.getData('text/plain'))
-        if (data.type === "Macro") {
-          // Save pack ("packname.pack")
-          // Save id ("id")
-        }
-        console.log(data);
-      }
-    } 
-  }).bind(html[0]);
-}
-
-function _onRenderDrawingConfig(app, html, data) {
-  if (true) { // Allowed to do this at all
-    _injectDrawingConfigTab(app, html, data); 
-    _supportDragDrop(html);
-  }
-}
-
-function _onPreUpdateDrawing(scene, drawing, update, options, userId) {
-  console.log(drawing);
-  console.log(update);
-}
-
-// Hooks.on('init', () => game.triggers = new TriggerHappy())
-Hooks.on("renderDrawingConfig", _onRenderDrawingConfig);
-Hooks.on("preUpdateDrawing", _onPreUpdateDrawing);
-console.log("HOTSPOTS: Added the hook");
+Hooks.on("init", () => {
+  new DrawingConfigManager(); 
+});

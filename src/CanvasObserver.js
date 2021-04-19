d63cfc4f9000da2c8a59d36e6b1d1e65e3508661
Kevin Gisi <kevin@kevingisi.com>
2021-04-19 17:04:56 -0500

    Added canvas observer, click macros are firing based on drawing bounding boxes

--- src/CanvasObserver.js
+++ src/CanvasObserver.js
@@ -1,62 +1,86 @@
 const Foundry = require("./utils/foundry");
+const MacroManager = require("./MacroManager");
 
 class CanvasObserver {
   static initialize() {
-    Foundry.Hooks.on("canvasReady", this.refreshBoardListener.bind(this));
-    // Foundry.Hooks.on('updateScene', this.refreshBoardListener.bind(this));
-    // on drawing added, keep it in the cache?
+    Foundry.Hooks.on("canvasReady", this.handleCanvasReady.bind(this));
+    ["updateScene", "updateDrawing", "deleteDrawing"].forEach((hook) => {
+      Foundry.Hooks.on(hook, this.updateActiveDrawingTargets.bind(this));
+    });
   }
 
-  static refreshBoardListener(a, b, diff, sceneId) {
-    console.log("Refreshing board listener");
-    const canvas = Foundry.canvas();
+  static handleCanvasReady() {
+    Foundry.canvas().stage.on("click", this.handleBoardClick.bind(this));
+    this.updateActiveDrawingTargets();
+  }
 
-    // https://github.com/League-of-Foundry-Developers/fvtt-module-trigger-happy/blob/master/trigger.js#L204
-    // Looks like we want to add our events on our own to canvas.stage, which is the parent pixi container to all the layers
+  static updateActiveDrawingTargets() {
+    this.activeDrawingTargets = Foundry.canvas().drawings.placeables.filter(
+      (drawing) => {
+        return (
+          this.drawingHasClickMacro(drawing) &&
+          this.macroEnabledForHiddenStatus(drawing)
+        );
+      }
+    );
+  }
 
-    // This intercepts everything on the canvas, but gives us precious little;
-    // we'll be rebuilding things from scratch
-    // document.getElementById('board').addEventListener('mouseup', (...args) => {
-    //   console.log('Hopefully we intercept everything now');
-    // });
+  static drawingHasClickMacro(drawing) {
+    const hotspots = drawing.data.flags.hotspots;
+    return (
+      hotspots &&
+      hotspots.click &&
+      hotspots.click.macro &&
+      hotspots.click.macro.length
+    );
+  }
 
-    // This doesn't fire when the user clicks tiles. Maybe that's fine?
-    // const original = canvas.mouseInteractionManager.callbacks.clickLeft;
-    // canvas.mouseInteractionManager.callbacks.clickLeft = (...args) => {
-    //   console.log("Maybe we intercept first");
-    //   original(...args);
-    // };
-
-    // This doesn't work much at all - they aren't clickable unless in the drawing tool
-    // Foundry.canvas().drawings.placeables.forEach(drawing => {
-
-    //   console.log("Manipulating drawing");
-    //   drawing.frame.handle.on("mouseup", () => {
-    //     console.log("Intercepted");
-    //   });
-    // });
+  static macroEnabledForHiddenStatus(drawing) {
+    return !(
+      drawing.data.flags.hotspots.click.disableHidden && drawing.data.hidden
+    );
   }
 
   static handleBoardClick(event) {
-    console.log("got click event from board");
+    if (!this.activeDrawingTargets.length) return;
+    if (Foundry.canvas().activeLayer === Foundry.canvas().drawings) return;
 
-    // const {x, y} = convertCoordsToMapCoords(event);
-    // if (!drawingBoundingBoxesInclude(x, y)) return;
-    // const intersectingDrawing = drawings.find(d => drawingContainsPoint(x, y));
-    // const macroCompositeId = intersectingDrawing.flags.hotspots.clickMacro;
-    // let macro;
-    // if (macroCompositeId.includes('@')) {
-    //   const [pack, macro] = macroCompositeId.split('@');
-    //   const compendium = game.packs.get(pack);
-    //   macro = await compendium.getEntity(macroCompositeId);
-    // } else {
-    //   macro = game.macros.get(macroCompositeId);
-    // }
-    // if (this.macroRequiresGM(macro) && !this.isTheGM()) {
-    //   this.requestGMMacro(macroCompositeId);
-    // } else {
-    //   this.executeMacro(macro);
-    // }
+    const point = this.getCanvasPoint(event);
+    const intersection = this.activeDrawingTargets.find((drawing) => {
+      return (
+        this.isPointInsideBoundingBox(point, drawing) &&
+        this.isPointInsideShape(point, drawing)
+      );
+    });
+
+    if (intersection) this.executeClickMacro(intersection);
+  }
+
+  static getCanvasPoint(event) {
+    const canvas = Foundry.canvas();
+    const { tx, ty } = canvas.tokens.worldTransform;
+    const { x, y } = canvas.stage.scale;
+    return {
+      x: (event.data.global.x - tx) / x,
+      y: (event.data.global.y - ty) / y,
+    };
+  }
+
+  static isPointInsideBoundingBox(point, drawing) {
+    const { width, height, x, y } = drawing.data;
+    return (
+      Number.between(point.x, x, x + width) &&
+      Number.between(point.y, y, y + height)
+    );
+  }
+
+  static isPointInsideShape(point, drawing) {
+    return true; // TODO
+  }
+
+  static async executeClickMacro(drawing) {
+    const { macro, pack } = drawing.data.flags.hotspots.click;
+    MacroManager.execute(await MacroManager.find(macro, pack));
   }
 }
 
